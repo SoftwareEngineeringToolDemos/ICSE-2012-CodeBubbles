da7bbd3a276a7ca42d907b2abd9e5fd24b994299
Andrew Kovacs <akovacs@cs.brown.edu>
2011-06-20 15:20:48 -0400

    Added stub class for Courses and a CourseRepository
    Former-commit-id: 5e8fafe07a6d36c4309ff529d86835139536d983

---
+++ javasrc/edu/brown/cs/bubbles/bgta/educhat/Course.java
@@ -0,0 +1,113 @@
+package edu.brown.cs.bubbles.bgta.educhat;
+
+import javax.swing.Icon;
+
+import edu.brown.cs.bubbles.bass.BassName;
+import edu.brown.cs.bubbles.bass.BassConstants.BassNameType;
+import edu.brown.cs.bubbles.buda.BudaBubble;
+import edu.brown.cs.bubbles.bump.BumpLocation;
+
+public class Course implements BassName {
+
+   @Override
+   public BudaBubble createBubble() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public BudaBubble createPreviewBubble() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String createPreviewString() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getClassName() {
+      return this.getClassName();
+   }
+
+   @Override
+   public Icon getDisplayIcon() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getDisplayName() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getFullName() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public BumpLocation getLocation() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public int getModifiers() {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+   @Override
+   public String getName() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String[] getNameComponents() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getNameHead() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public BassNameType getNameType() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getNameWithParameters() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getPackageName() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public String getProject() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public int getSortPriority() {
+      // TODO Auto-generated method stub
+      return 0;
+   }
+
+}
---
+++ javasrc/edu/brown/cs/bubbles/bgta/educhat/CourseRepository.java
@@ -0,0 +1,21 @@
+package edu.brown.cs.bubbles.bgta.educhat;
+
+import java.util.List;
+
+import edu.brown.cs.bubbles.bass.BassName;
+import edu.brown.cs.bubbles.bass.BassConstants.BassRepository;
+
+public class CourseRepository implements BassRepository {
+   //private List<Course> courses;
+   @Override
+   public Iterable<BassName> getAllNames() {
+      // TODO Auto-generated method stub
+      return null;
+   }
+
+   @Override
+   public boolean includesRepository(BassRepository br) {
+      // TODO Auto-generated method stub
+      return false;
+   }
+}

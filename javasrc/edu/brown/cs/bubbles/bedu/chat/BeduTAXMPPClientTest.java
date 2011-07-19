2e1caa5bcef9b3b699abc26c68693bc50f97495d
Andrew Kovacs <akovacs@cs.brown.edu>
2011-07-19 14:04:11 -0400

    Added test for message routing, it fails right now
    Former-commit-id: 713bcd74110c0c8aef1ce7067863beae5eb4a0d9

--- javasrc/edu/brown/cs/bubbles/bedu/chat/BeduTAXMPPClientTest.java
+++ javasrc/edu/brown/cs/bubbles/bedu/chat/BeduTAXMPPClientTest.java
@@ -19,22 +19,20 @@
 
 package edu.brown.cs.bubbles.bedu.chat;
 
-import java.util.Scanner;
 
-import junit.framework.TestCase;
 import static junit.framework.Assert.*;
-import org.junit.Before;
+
+import java.util.Date;
+
 import org.junit.BeforeClass;
 import org.junit.AfterClass;
 import org.junit.Test;
-import org.junit.After;
 import org.jivesoftware.smack.Chat;
 import org.jivesoftware.smack.ConnectionConfiguration;
 import org.jivesoftware.smack.MessageListener;
 import org.jivesoftware.smack.XMPPConnection;
 import org.jivesoftware.smack.XMPPException;
 import org.jivesoftware.smack.packet.Message;
-import org.jivesoftware.smack.util.StringUtils;
 
 import edu.brown.cs.bubbles.bedu.chat.BeduCourse.TACourse;
 
@@ -43,16 +41,24 @@ private static BeduTAXMPPClient ta_client;
 private static BeduTAXMPPClient ta_client2;
 private static BeduTAXMPPClient ta_client3;
 private static XMPPConnection	student_conn1;
+private static XMPPConnection student_conn2;
 
 // login names
 private static String			  ta_login		= "codebubbles@jabber.org";
 private static String			  student_login = "codebubbles2";
+private static String           student2_login = "codebubbles3";
 
+//result bools for the routing test that have
+//to be members so we can set them inside
+//anonymous message listeners
+private boolean s1ToT2Received = false;
+private boolean t2ToS1Received = false;
+private boolean t1ToS2Received = false;
+private boolean s2ToT1Received = false;
 
 
 @BeforeClass public static void setUpOnce() throws XMPPException {
-	System.out.println("Setting up once");
-	//BeduCourse.TACourse c = new TACourse("testcourse", ta_login, "brownbears", "jabber.org");
+   //BeduChatFactory.DEBUG = true;
 	ta_client = new BeduTAXMPPClient(new TACourse("testcourse", ta_login, "brownbears", "jabber.org"));
 	ta_client.connectAndLogin("TA1");
 
@@ -110,7 +116,6 @@ private static String			  student_login = "codebubbles2";
 	assertTrue(ta_client.getTickets().size() == 1);
 	BeduStudentTicket t = ta_client.getTickets().get(0);
 	assertEquals(t.getText(), "this is a ticket");
-	//assertEquals(t.getStudentJID(), student_login + "@jabber.org/Smack");
 	
 	ta_client.acceptTicketAndAlertPeers(t);
 	assertTrue(ta_client.getTickets().size() == 0);
@@ -118,13 +123,14 @@ private static String			  student_login = "codebubbles2";
 
 @Test public void testTicketForwardAndAccept() throws XMPPException, InterruptedException
 {
+   System.out.println("Testing forward and accept");
    Chat c = student_conn1.getChatManager().createChat("codebubbles@jabber.org/TA1", null);
    ta_client2.connectAndLogin("TA2");
    
    assertTrue(ta_client.getTickets().size() == 0);
    assertTrue(ta_client2.getTickets().size() == 0);
    
-   c.sendMessage("TICKET:derp");
+   c.sendMessage("TICKET:tick");
    Thread.sleep(5000);
    assertTrue(ta_client.getTickets().size() == 1);
    assertTrue(ta_client2.getTickets().size() == 1);
@@ -153,16 +159,107 @@ private static String			  student_login = "codebubbles2";
    assertTrue(ta_client3.getTickets().size() == 0);
    
    ta_client3.connectAndLogin("TA3");
-   Thread.sleep(2000);
+   Thread.sleep(3000);
    assertTrue(ta_client.getTickets().size() == 2);
    assertTrue(ta_client3.getTickets().size() == 2);
    ta_client.acceptTicketAndAlertPeers(ta_client.getTickets().get(0));
    ta_client3.acceptTicketAndAlertPeers(ta_client.getTickets().get(0));
-   Thread.sleep(2000);
+   Thread.sleep(3000);
    assertTrue(ta_client.getTickets().size() == 0);
    assertTrue(ta_client3.getTickets().size() == 0);
    
 }
 
+@Test public void testMessageRouting() throws XMPPException, InterruptedException
+{
+   /**
+    * With 2 TAs and 2 student connections have the two students
+    * send tickets to different TAs, have TAs accept tickets from students
+    * from whom they didn't receive the intial ticket and then try to send messages
+    * in both directions once the sessions are established and check if the 
+    * messages reach their destinations 
+    */
+   System.out.println("Testing correct routing of messages");
+   assertTrue(ta_client.getTickets().size() == 0);
+   assertTrue(ta_client2.getTickets().size() == 0);
+   
+   ConnectionConfiguration config = new ConnectionConfiguration("jabber.org", 5222);
+   config.setSecurityMode(ConnectionConfiguration.SecurityMode.required);
+   config.setSendPresence(true);
 
+   student_conn2 = new XMPPConnection(config);
+   student_conn2.connect();
+   student_conn2.login(student2_login, "brownbears");
+
+   
+   
+   Chat s1ToT2 = student_conn1.getChatManager().createChat("codebubbles@jabber.org", new MessageListener()
+   {
+      @Override
+      public void processMessage(Chat c, Message m)
+      {
+         if(m.getBody().equals("t2ToS1"))
+             t2ToS1Received = true;   
+      }  
+   });
+   
+   Chat s2ToT1 = student_conn2.getChatManager().createChat("codebubbles@jabber.org", new MessageListener(){
+      @Override
+      public void processMessage(Chat c, Message m)
+      {
+         if(m.getBody().equals("t1ToS2"))
+            t1ToS2Received = true;
+      }
+   });
+   
+   student_conn1.getChatManager().createChat("codebubbles@jabber.org/TA1", null).sendMessage("TICKET:s1ToT2"); //hashcode = -954750633
+   Thread.sleep(1000);
+   student_conn2.getChatManager().createChat("codebubbles@jabber.org/TA2",null).sendMessage("TICKET:s2ToT1"); //hash = -953827113
+
+   Thread.sleep(3000);
+   assertTrue(ta_client.getTickets().size() == 2);
+   assertTrue(ta_client2.getTickets().size() == 2);
+   
+   ta_client.acceptTicketAndAlertPeers(new BeduStudentTicket("s2ToT1", new Date(), student2_login + "@jabber.org"));
+   
+   ta_client2.acceptTicketAndAlertPeers(new BeduStudentTicket("s1ToT2", new Date(), student_login + "@jabber.org"));
+   Thread.sleep(3000);
+   assertTrue(ta_client.getTickets().size() == 0);
+   assertTrue(ta_client2.getTickets().size() == 0);
+   
+   Chat t1ToS2 = ta_client.getChatForJID(student2_login + "@jabber.org");
+   Chat t2ToS1 = ta_client2.getChatForJID(student_login + "@jabber.org");
+   
+   t1ToS2.addMessageListener(new MessageListener() {
+     @Override
+      public void processMessage(Chat c, Message m)
+      {
+         if(c.getParticipant().equals(student2_login + "@jabber.org"))
+            s2ToT1Received = true;
+      } 
+   });
+   
+   t2ToS1.addMessageListener(new MessageListener() {
+      @Override
+      public void processMessage(Chat c, Message m)
+      {
+         if(c.getParticipant().equals(student_login + "@jabber.org"))
+            s1ToT2Received = true;
+      } 
+   });
+   
+   s1ToT2.sendMessage("s1toT2");
+   Thread.sleep(1000);
+   s2ToT1.sendMessage("s2toT1");
+   Thread.sleep(1000);
+   t2ToS1.sendMessage("t2ToS1");
+   Thread.sleep(1000);
+   t1ToS2.sendMessage("t1ToS2");
+   Thread.sleep(10000);
+   
+   assertTrue(s1ToT2Received);
+   assertTrue(s2ToT1Received);
+   assertTrue(t2ToS1Received);
+   assertTrue(t1ToS2Received);
+}
 }
